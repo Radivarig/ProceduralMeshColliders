d937d8edc4c1ed956952cbab0ea8bcc89cc872ff
Radivarig <reslav.hollos@gmail.com>
2014-10-01 22:19:59 +0200

    add: procedural extrude as floors; add: floor values; add: tris connected;

--- ProceduralMesh.cs
+++ ProceduralMesh.cs
@@ -5,23 +5,36 @@ using System.Collections.Generic;
 //[ExecuteInEditMode]
 public class ProceduralMesh : MonoBehaviour {
 
-	public float checkEvery = 2f;
+	public enum Types{
+		Ngon,
+		Piramid,
+		Prism,
+		Dome,
+	}
+	//public Types ;
+	public Types type = Types.Prism;
+
+	public float checkEvery = 0.2f;
 
 	//base
-	[Range(3, 32)] public int baseNumber;
-	[Range(0.01f, 10f)]public float radius = 1f;
+	[Range(3, 32)] public int baseNumber = 3;
+	[Range(1, 5)] public int floorNumber = 2;
+
+	public List<PairFloat> floorValues = new List<PairFloat>();
 
 	//mesh
 	private MeshFilter mf;
 	private Mesh mesh;
+	private MeshCollider mc;
 
-	public Vector3[] vertices;
-	public Vector2[] uv;
-	public int[] triangles;
+	/*public Vector3[] vertices;
+	public int[] triangles;*/
 
 	void Start () {
 		InitMesh();
-		gameObject.AddComponent<MeshRenderer>();
+		//gameObject.AddComponent<MeshRenderer>();
+		mc = gameObject.AddComponent<MeshCollider>();
+		mc.convex = true;
 	}
 
 	void InitMesh(){
@@ -38,40 +51,110 @@ public class ProceduralMesh : MonoBehaviour {
 
 	void Update () {
 		if (TrueEverySeconds(checkEvery) && InspectorChanged()){
-			CreateMesh();
-			ConnectVerticesWithCenter();
-			RefreshMesh();
+			while (floorValues.Count < floorNumber)
+				floorValues.Add(new PairFloat());
 
+		 	if(type == Types.Ngon) MakeNgon(true);
+		 	else if(type == Types.Prism) MakePrism();
+			else mesh.Clear();
+
+			//mc.sharedMesh = mesh;
+			mc.enabled = false;
+			mc.enabled = true;
 		}
 	}
 
-	void ConnectVerticesWithCenter(){
+	//public void MakePrism(bool flipNormals = false){
+	public void MakePrism(){
+		List<Vector3> vertices = new List<Vector3>();
+
+		foreach(PairFloat floor in floorValues){
+			floor.vertices = BaseVertices(baseNumber, floor.radius, floor.height);
+			vertices.AddRange(floor.vertices);
+		}
+
 		List<int> tris = new List<int>();
-		for (int i = 1; i < vertices.Length -1; i++){
-			tris.Add(0);
+		int floorCount = floorValues[0].vertices.Count;
+		for (int i = 0; i < floorCount -1 ; i++){
+			for(int j = 0; j < floorNumber-1; j++){
+				int k = j*floorCount + i;
+				tris.Add(k + floorCount);
+				tris.Add(k + 1);
+				tris.Add(k);
+
+				tris.Add(k + floorCount);
+				tris.Add(k + floorCount + 1);
+				tris.Add(k + 1);
+			}
+		}
+
+		for(int j = 0; j < floorNumber-1; j++){
+			int k = j*floorCount + floorCount -1;
+			tris.Add(k + floorCount);	//last up
+			tris.Add(k -floorCount +1);	//first
+			tris.Add(k);	//last
+			
+			tris.Add(k + 1);	//first up //k -floorCount +1 - floorCount
+			tris.Add(k - floorCount +1);
+			tris.Add(k + floorCount);
+		}
+
+		mesh.Clear();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = tris.ToArray();
+	}
+
+	public void MakeNgon(bool flipNormals = false){
+		List<Vector3> baseVerts = BaseVertices(baseNumber, floorValues[0].radius, floorValues[0].height);
+
+		baseVerts.Add(new Vector3(0f, floorValues[0].height, 0f));	//center
+
+		List<int> tris = MakeTrianglesWithCenter(baseVerts);
+
+		mesh.Clear();
+		
+		mesh.vertices = baseVerts.ToArray();
+		mesh.triangles = tris.ToArray();
+
+		if(flipNormals) FlipNormals(mesh);
+
+	}
+	void CreateMesh(){
+
+
+	}
+
+	//TODO maybe add flip, not important
+	List<int> MakeTrianglesWithCenter(List<Vector3> baseVerts, float height = 0f){
+		//TODO if height = 0f dont make it in center to minimize mesh
+
+		//baseVerts.Add(new Vector3(0f, height, 0f));	//center
+		int count = baseVerts.Count;
+
+		List<int> tris = new List<int>();
+		for (int i = 0; i < count -1; i++){
+			tris.Add(count -1);
 			tris.Add(i);
 			tris.Add(i+1);
 		}
+		tris.Add(count -2);
 		tris.Add(0);
-		tris.Add(vertices.Length-1);
-		tris.Add(1);
+		tris.Add(count -1);
 
-		triangles = tris.ToArray();
+		return tris;
 	}
 
-	void CreateMesh(){
+	List<Vector3> BaseVertices(int baseNumber, float radius = 1f, float heigth = 0f){
 		List<Vector3> verts = new List<Vector3>();
-		//center
-		verts.Add(Vector3.zero);
 
 		for(int i = 0; i < baseNumber; i++){
 			float radians = i * 360f/baseNumber * Mathf.Deg2Rad;
 			float x = Mathf.Cos(radians)*radius;
 			float z = Mathf.Sin(radians)*radius;
-			verts.Add(new Vector3 (x, 0f, z));
+			verts.Add(new Vector3 (x, heigth, z));
 		}
-
-		vertices = verts.ToArray();
+	
+		return verts;
 	}
 
 	//variable needed only for TrueEverySeconds
@@ -96,11 +179,36 @@ public class ProceduralMesh : MonoBehaviour {
 		return true;
 	}
 	
-	public void RefreshMesh(){
+	/*public void RefreshMesh(){
 		mesh.Clear();
 
 		mesh.vertices = vertices;
 		mesh.uv = uv;
 		mesh.triangles = triangles;
+	}*/
+
+	void FlipNormals(Mesh mesh){
+		List<int> triangles = new List<int>();
+		triangles.AddRange(mesh.triangles);
+
+		for(int i = 1; i < triangles.Count -1; i++){
+			int temp = triangles[i-1];
+			triangles[i-1] = triangles[i+1];
+			triangles[i+1] = temp;
+			i += 2;
+		}
+		mesh.triangles = triangles.ToArray();
+	}
+}
+
+[System.Serializable]
+public class PairFloat{
+	public List<Vector3> vertices;
+	[Range(0.01f, 3f)]public float radius;
+	[Range(0, 3)] public float height;
+
+	public PairFloat(float radius = 1f, float height = 0f){
+		this.radius = radius;
+		this.height = height;
 	}
 }
